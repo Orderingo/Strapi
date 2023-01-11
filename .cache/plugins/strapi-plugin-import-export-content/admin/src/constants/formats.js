7f6a518eefbde0afcc98b27e61143bdcc2d9efcc
Usman Habeeb <usman.habeeb@aajtech.com>
2023-01-12 00:06:16 +0500

    deletes

--- .cache/plugins/strapi-plugin-import-export-content/admin/src/constants/formats.js
+++
@@ -1,7 +0,0 @@
-const FORMATS = [
-  { name: "csv", mimeType: "text/csv", ext: ".csv" },
-  { name: "csv-excel", mimeType: "application/vnd.ms-excel", ext: ".csv" },
-  { name: "json", mimeType: "application/json", ext: ".json" },
-];
-
-export default FORMATS;
